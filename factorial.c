82142ac214cd62cc6f86e84d52c91ae775172579
pathakjagriti091-design <pathakjagriti091@gmail.com>
2026-05-28 14:00:21 +0530

    added factorial program

---
+++ factorial.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n, i ,fact =1;
+    printf(" enter the num :");
+    scanf("%d", &n);
+    for (i=1; i<=n; i++){
+        fact = fact*i;
+    }
+     printf("factorial =%d",fact);
+     return 0;
+}
